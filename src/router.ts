d63839d397748c94df4ad60dda8f04e6eec9859f
Jesica Dominguez <jesy.dom19@gmail.com>
2025-11-19 20:05:15 -0600

    Modifico router para usar hash routing

--- src/router.ts
+++ src/router.ts
@@ -6,16 +6,17 @@ import { initPageComputerYouChoice } from "./pages/computer-you-choice/computer-
 import { initPageResult } from "./pages/result/result";
 
 const routes = [
-  { path: /\/welcome/, page: initPageWelcome },
-  { path: /\/instructions/, page: initPageInstructions },
-  { path: /\/choice/, page: initPageChoice },
-  { path: /\/computer-you-choice/, page: initPageComputerYouChoice },
-  { path: /\/result/, page: initPageResult },
+  { path: /#\/welcome/, page: initPageWelcome },
+  { path: /#\/instructions/, page: initPageInstructions },
+  { path: /#\/choice/, page: initPageChoice },
+  { path: /#\/computer-you-choice/, page: initPageComputerYouChoice },
+  { path: /#\/result/, page: initPageResult },
 ];
 
 export function initRouter(container: Element) {
   function goTo(path) {
-    history.pushState({}, "", path); //pushState para que no se recargue la página, cambia la ruta despues de la barra / en el navegador
+    /* history.pushState({}, "", path); */ //pushState para que no se recargue la página, cambia la ruta despues de la barra / en el navegador
+    location.hash = path;
     handleRoute(path);
   }
 
@@ -34,9 +35,21 @@ export function initRouter(container: Element) {
     }
   }
 
-  if (location.pathname === "/") {
+  // Escuchar cambios en el hash
+  window.addEventListener("hashchange", () => {
+    handleRoute(location.hash);
+  });
+
+  // Inicializar en la ruta de bienvenida
+  if (location.hash === "") {
+    goTo("/welcome");
+  } else {
+    handleRoute(location.hash);
+  }
+
+  /*  if (location.pathname === "/") {
     goTo("/welcome");
   } else {
     handleRoute(location.pathname);
-  }
+  } */
 }
